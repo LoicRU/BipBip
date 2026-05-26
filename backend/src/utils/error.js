647b60c47b53919fb680c39a27d683599d2d074d
loicRU <loic.ruyssen@epitech.eu>
2026-05-26 18:10:33 +0200

    .

--- backend/src/utils/error.js
+++
@@ -1,7 +0,0 @@
-export class AppError extends Error {
-  constructor(statusCode, message, details = undefined) {
-    super(message);
-    this.statusCode = statusCode;
-    this.details = details;
-  }
-}
